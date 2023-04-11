0e6a57e1ecd8cb802c7c823a5cbca4c61c51228f
ARTR <trejoramon141295@gmail.com>
2023-04-10 20:56:46 -0600

    Se agrego sistema de columnas y el modal

--- js/fetch.js
+++ js/fetch.js
@@ -42,17 +42,41 @@ let mainProds = document.getElementById("mainProds");
 function createCards(prods){
     prods.forEach(prod => {
         mainProds.insertAdjacentHTML("beforeend",
-        `   <div class="card col" style="width: 18rem;">
-                <img src="${prod.image}" class="card-img-top" alt="${prod.description}"
-                style="max-width:15rem;height:15rem;>
-                <div class="card-body">
-                    <h5 class="card-title">${prod.title}</h5>
-                    <p class="card-text"><strong>${prod.category}</strong></p>
-                    <p class="card-text">${prod.description.slice(0,20)} ...</p>
-
-
+        `   <div class="col-12 col-md-6 col-lg-4" >
+                <div class="card" style="width: 18rem;" >
+                    <img src="${prod.image}" class="card-img-top" alt="${prod.description}"
+                    style="max-width: 15rem; height: 15rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">${prod.title}</h5>
+                        <p class="card-text"><strong>${prod.category}</strong></p>
+                        <p class="card-text">${prod.description.slice(0,20)} ...</p>
+                        <button type="button" class="btn btn-primary" 
+                        data-bs-toggle="modal" 
+                        data-bs-target="#exampleModal_${prod.id}">
+                        Mas Info
+                        </button>
+                    </div>
                 </div>
-
+            </div> <!-- div Cards -->                    
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal_${prod.id}" 
+            tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">${prod.title}</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            ${prod.description}
+                            <p class=text-end><strong> $ ${prod.price} USD </strong></p>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                                Cerrar</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         `)
     });
